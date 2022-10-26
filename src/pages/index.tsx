78a74e02056e63823aa9e688ffc0220a71c9b3eb
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-25 22:03:43 -0300

    Inclusão de texto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
      <h1>Sujeito Pizza</h1>
     </div>
   )
