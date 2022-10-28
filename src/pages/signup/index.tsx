f52fe38a553ed9151eab968b6dea3d7099a6b202
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-27 21:55:59 -0300

    Inclusão de texto

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import Image from 'next/image';
+import styles from '../../../styles/home.module.scss';
+
+import logoImg from '../../../public/logo.svg';
+
+import { Input } from '../../components/ui/Input'
+import { Button } from '../../components/ui/Button'
+
+import Link from 'next/link';
+
+export default function SignUp() {
+  return (
+    <>
+    <Head>
+      <title>SujeitoPizza - Faça seu login</title>
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
+      <div className={styles.login}>
+        <form>
+           <Input
+             placeholder="Digite seu email"
+             type="text"
+           />
+
+           <Input
+             placeholder="Sua senha"
+             type="password"
+           />
+
+           <Button
+             type="submit"
+             loading={false}
+           >
+            Acessar
+           </Button>
+          </form>
+
+          <Link href="/signup">
+             <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          </Link>
+
+
+      </div>
+    </div>
+    </>
+  )
+}
