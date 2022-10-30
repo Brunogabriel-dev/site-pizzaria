fafb06bea04ff101353b9e898091ff8e19aad52a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-30 11:09:38 -0300

    Inclusão de texto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext, FormEvent } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
@@ -7,9 +9,24 @@ import logoImg from '../../public/logo.svg';
 import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
 
+import { AuthContext } from '../contexts/AuthContext'
+
 import Link from 'next/link';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    let data = {
+      email: "algum@teste.com",
+      password: "123123"
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
     <Head>
@@ -19,7 +36,7 @@ export default function Home() {
       <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
       <div className={styles.login}>
-        <form>
+        <form onSubmit={handleLogin}>
            <Input
              placeholder="Digite seu email"
              type="text"
