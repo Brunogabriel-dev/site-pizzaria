ac3cbbe7beccaaeef111431b8396b77e385bf4c4
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-05 20:54:44 -0300

    Inclusão de texto

--- src/utils/canSSRGuest.ts
+++ src/utils/canSSRGuest.ts
@@ -3,7 +3,22 @@ import { parseCookies } from 'nookies'
 
 //funcao para paginas que só pode ser acessadas por visitantes
 export function canSSRGuest<p>(fn: GetServerSideProps<p>) {
-  return async (ctx: GetServerSidePropsContext ) => {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<p>> => {
+
+    const cookies = parseCookies(ctx);
+
+    // Se o cara tentar acessar a pagina porem tendo já um login salvo redirecionamos
+    if(cookies['@nextauth.token']){
+      return {
+        redirect:{
+          destination: '/dashboard'
+        }
+      }
+    }
+
+
+
+    return await fn(ctx);
 
   }
 }
