972aa2f7bcb013ad66a453ceee84e94227c508ca
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-05 21:16:19 -0300

    Inclusão de texto

--- src/utils/canSSRAuth.ts
+++ src/utils/canSSRAuth.ts
@@ -4,5 +4,21 @@ import { parseCookies, destroyCookie } from 'nookies'
 
 // funcao para paginas que só users logados podem ter acesso.
 export function canSSRAuth<p>(fn: GetServerSideProps<p>){
-  
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<p>> => {
+    const cookies = parseCookies(ctx);
+
+    const token = cookies['@nextauth.token'];
+
+    if(!token){
+      return{
+        redirect:{
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
+
+    
+
+  }
 }
