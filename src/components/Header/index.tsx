d742f98b30b0b7186051236007f4d2729faed7a7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-06 14:45:43 -0300

    Inclusão de texto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import styles from './styles.module.scss'
+import Link from 'next/link'
 
 export function Header(){
   return(
-    <header>
-      <h1>HEEADER</h1>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+
+      </div>
+      
     </header>
   )
 }
