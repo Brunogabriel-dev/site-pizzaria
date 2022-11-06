5b6428bb5a5a1761750033b9f4a6942b4b7fcd39
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-06 14:58:39 -0300

    Inclusão de texto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,19 @@ export function Header(){
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        <Link href="/dashboard">
+        <img src="/logo.svg" width={190} height={60} />
+        </Link>
+
+        <nav>
+          <Link href="/category">
+          <a>Categoria</a>
+          </Link>
+        </nav>
+
+
 
       </div>
-      
     </header>
   )
 }
