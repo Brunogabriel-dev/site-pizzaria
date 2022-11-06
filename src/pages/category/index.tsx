dd03d290265653eea346269ba2897e36bf7076a8
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-06 16:41:43 -0300

    Inclusão de texto

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,8 +1,17 @@
+import { useState, FormEvent } from 'react'
 import Head from "next/head"
 import { Header } from '../../components/Header'
 import styles from './styles.module.scss'
 
 export default function Category(){
+  const [name, setName] = useState('')
+
+  async function handleRegister(event: FormEvent){
+    event.preventDefault();
+
+    alert("CATEGORIA" + name)
+  }
+
   return(
     <>
     <Head>
@@ -14,11 +23,13 @@ export default function Category(){
       <main className={styles.container}>
         <h1>Cadastrar categorias</h1>
 
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleRegister}>
           <input 
           type="text"
           placeholder="Digite o nome da categoria"
           className={styles.input}
+          value={name}
+          onChange={ (e) => setName(e.target.value) }
            />
 
            <button className={styles.buttonAdd} type="submit">
