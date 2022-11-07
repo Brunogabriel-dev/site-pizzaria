54e37e11ff5c7aed32e3679cedda4e6ffc282464
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-07 20:57:31 -0300

    Inclusão de texto

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,44 @@
+
+import Head from "next/head";
+import styles from './styles.module.scss';
+import { Header } from '../../components/Header'
+
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
+
+export default function Product(){
+  return(
+    <>
+    <Head>
+      <title> Novo produto - Sujeito Pizzaria</title>
+    </Head>
+    <div>
+      <Header/>
+
+      <main className={styles.container}>
+        <h1>Novo produto</h1>
+
+        <form className={styles.form}>
+
+
+          <select>
+            <option>
+              Bebida
+            </option>
+          </select>
+
+        </form>
+
+      </main>
+      
+    </div>
+    </>
+  )
+}
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+  return{
+    props: {}
+  }
+})
