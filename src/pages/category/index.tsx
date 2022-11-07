b0365d0cacd018be4da83e52539eb1d70bc90a25
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-07 20:39:37 -0300

    Inclusão de texto

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -6,6 +6,8 @@ import styles from './styles.module.scss'
 import { setupAPIClient } from '../../services/api'
 import { toast } from 'react-toastify'
 
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
 export default function Category(){
   const [name, setName] = useState('')
 
@@ -57,3 +59,12 @@ export default function Category(){
     </>
   )
 }
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+  return {
+    props: {}
+  }
+  
+})
