a66760d11da2fc03b7ce6e018ba4bf6612498fe9
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-07 21:13:37 -0300

    Inclusão de texto

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -30,11 +30,26 @@ export default function Product(){
             </option>
           </select>
 
-          <input type="text"
+          <input 
+          type="text"
           placeholder="Digite o nome do produto"
+          className={styles.input}
            />
 
+          <input 
+          type="text"
+          placeholder="Preço do produto"
+          className={styles.input}
+          />
 
+          <textarea
+            placeholder="Descreva seu produto..."
+            className={styles.input}
+          />
+
+          <button className={styles.buttonAdd} type="submit">
+            Cadastrar
+          </button>
 
         </form>
 
