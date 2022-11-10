3499609dc1bdebeb96eb26b20e9a4851827001c6
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-09 22:34:53 -0300

    Inclusão de texto

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { canSSRAuth } from '../../utils/canSSRAuth'
 import Head from 'next/head'
-import styles from './styles.module.scss';
+import styles from './styles.module.scss'
 
 import { Header } from '../../components/Header'
 import { FiRefreshCcw } from 'react-icons/fi'
@@ -23,9 +23,17 @@ export default function Dashbord(){
           </button>
         </div>
 
-        <article className={styles.listOreders}>
+        <article className={styles.listOrders}>
+
+          <section className={styles.orderItem}>
+            <button>
+              <div className={styles.tag}></div>
+              <span>Mesa 30</span>
+            </button>
+          </section>
 
         </article>
+
       </main>
  
     </div>
